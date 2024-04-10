63892737d2b3ae1bf550fb79eef0a0796b670c63
Jyri Högman <jyri.hogman@reaktor.com>
2024-04-10 22:20:24 +0300

    Improve tests

--- src/tests/service_tests.rs
+++ src/tests/service_tests.rs
@@ -27,6 +27,15 @@ fn create_pricing_with_hour(hour: u32, price: f32) -> Pricing {
     }
 }
 
+fn create_pricing_with_hour_and_day(hour: u32, day: u32, price: f32) -> Pricing {
+    let offset = FixedOffset::east_opt(0).unwrap();
+    let date_time = offset.with_ymd_and_hms(2024, 4, day, hour, 0, 0).unwrap();
+    Pricing {
+        date_time,
+        price_no_tax: price,
+    }
+}
+
 #[test]
 fn test_get_filtered_pricing_normal_period() {
     let pricing_data = vec![
@@ -54,15 +63,18 @@ fn test_get_filtered_pricing_cross_midnight() {
         create_static_pricing_with_hour(3),
         create_static_pricing_with_hour(4),
         create_static_pricing_with_hour(5),
+        create_static_pricing_with_hour(6),
+        create_static_pricing_with_hour(7),
+        create_static_pricing_with_hour(8),
     ];
     let pricing = Arc::from(pricing_data.into_boxed_slice());
     let starting_hour = 22;
-    let ending_hour = 5;
+    let ending_hour = 7;
 
     let filtered_pricing = get_filtered_pricing(&pricing, &starting_hour, &ending_hour);
-    assert_eq!(filtered_pricing.len(), 7);
+    assert_eq!(filtered_pricing.len(), 9);
     assert_eq!(filtered_pricing[0].date_time.hour(), 22);
-    assert!(filtered_pricing.iter().any(|&p| p.date_time.hour() == 2));
+    assert_eq!(filtered_pricing.last().unwrap().date_time.hour(), 6);
 }
 
 #[test]
@@ -96,16 +108,15 @@ fn test_calculate_cheapest_start_time() {
 #[test]
 fn test_calculate_cheapest_start_time_before_midnight() {
     let pricing_data = vec![
-        create_pricing_with_hour(22, 0.01),
-        create_pricing_with_hour(23, 0.02),
-        create_pricing_with_hour(0, 0.004),
-        create_pricing_with_hour(1, 0.1),
-        create_pricing_with_hour(2, 0.2),
-        create_pricing_with_hour(3, 0.6),
-        create_pricing_with_hour(4, 0.7),
-        create_pricing_with_hour(5, 0.8),
-        create_pricing_with_hour(6, 0.9),
-        create_pricing_with_hour(7, 0.6),
+        create_pricing_with_hour_and_day(22, 9, 4.722),
+        create_pricing_with_hour_and_day(23, 9, 4.078),
+        create_pricing_with_hour_and_day(0, 10, 0.619),
+        create_pricing_with_hour_and_day(1, 10, 0.869),
+        create_pricing_with_hour_and_day(2, 10, 0.508),
+        create_pricing_with_hour_and_day(3, 10, 0.107),
+        create_pricing_with_hour_and_day(4, 10, 0.000),
+        create_pricing_with_hour_and_day(5, 10, -0.001),
+        create_pricing_with_hour_and_day(6, 10, 0.000),
     ];
     let pricing_refs: Vec<&Pricing> = pricing_data.iter().collect();
 
@@ -116,7 +127,7 @@ fn test_calculate_cheapest_start_time_before_midnight() {
         Some(
             FixedOffset::east_opt(0)
                 .unwrap()
-                .with_ymd_and_hms(2024, 4, 8, 22, 0, 0)
+                .with_ymd_and_hms(2024, 4, 10, 1, 0, 0)
                 .unwrap()
         )
     );
