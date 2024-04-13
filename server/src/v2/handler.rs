f5bf40d128092cf4d12dc975b624b4dd575fbcd3
Jyri Högman <jyri.hogman@reaktor.com>
2024-04-13 21:41:55 +0300

    Improve endpoint path
    Country code as path parameter and hours, start & end as query parameters

--- server/src/v2/handler.rs
+++ server/src/v2/handler.rs
@@ -1,7 +1,7 @@
 use std::net::SocketAddr;
 
 use axum::{
-    extract::{ConnectInfo, Path},
+    extract::{ConnectInfo, Path, Query},
     http::StatusCode,
     response::IntoResponse,
 };
@@ -12,7 +12,8 @@ use super::service::is_water_heater_enabled_for_current_hour;
 
 pub async fn handle_enable_water_heater(
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
-    Path((country_code, hours, starting_hour, ending_hour)): Path<(BiddingZone, u32, u32, u32)>,
+    Path(country_code): Path<BiddingZone>,
+    Query((hours, starting_hour, ending_hour)): Query<(u32, u32, u32)>,
 ) -> impl IntoResponse {
     let is_enabled =
         is_water_heater_enabled_for_current_hour(country_code, hours, starting_hour, ending_hour)
--- server/src/v2/router.rs
+++ server/src/v2/router.rs
@@ -8,7 +8,7 @@ use super::handler::handle_enable_water_heater;
 pub fn v2_routes() -> Router {
     Router::new()
         .route(
-            "/waterheater/country/:country_code/hours/:hours/start/:starting_hour/end/:ending_hour",
+            "/waterheater/country/:country_code/cheapest-period",
             get(handle_enable_water_heater),
         )
         .fallback(not_found)
